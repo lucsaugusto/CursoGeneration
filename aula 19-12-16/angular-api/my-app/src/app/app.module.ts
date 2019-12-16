3ed0f66961e244f31f46cdb37727d11136d705df
lucsaugusto <lucasaugustoneves@hotmail.com>
2019-12-16 16:13:23 -0300

    Adicionando o front-end

--- aula 19-12-16/angular-api/my-app/src/app/app.module.ts
+++ aula 19-12-16/angular-api/my-app/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { PostListComponent } from './component/post-list/post-list.component';
-import { PostIdComponent } from './component/post-id/post-id.component'
+import { PostIdComponent } from './component/post-id/post-id.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NavComponent } from './component/nav/nav.component';
+import { FooterComponent } from './component/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostIdComponent
+    PostIdComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
